8cbce6740b30599bcf35136feafafa504db16fbc
Dolev Levy <Dolevlevy97@gmail.com>
2024-05-30 14:42:29 +0300

    Immplement book details modal dialog

--- Ex - Book Shop/js/book.controller.js
+++ Ex - Book Shop/js/book.controller.js
@@ -9,7 +9,7 @@ function renderBooks() {
     <td>${book.title}</td>
     <td>$${book.price}</td>
     <td class="btn-td">
-        <button class="btn btn-read">Read</button>
+        <button class="btn btn-read" onclick="onReadBook('${book.id}')">Read</button>
         <button class="btn btn-update" onclick="onUpdateBook('${book.id}')">Update</button>
         <button class="btn btn-delete" onclick="onRemoveBook('${book.id}')" >Delete</button>
     </td>
@@ -36,6 +36,17 @@ function onAddBook() {
     var title = prompt(`I'm glad you want to add a new book! What will be its title?`)
     while (!title) title = prompt(`Please enter a non-empty title for the book`)
     var price = +prompt(`Great! And its price?`)
+    while (price === NaN || !price) price = +prompt(`Please enter a number for the price`)
     addBook(title, price)
     renderBooks()
+}
+
+function onReadBook(id) {
+    const elDialog = document.querySelector('dialog')
+    const elPre = elDialog.querySelector('pre')
+
+    var book = gBooks.find((book) => book.id === id)
+
+    elPre.innerHTML = JSON.stringify(book)
+    elDialog.showModal()
 }
