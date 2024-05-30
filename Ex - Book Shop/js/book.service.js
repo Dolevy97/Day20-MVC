30756f2d2c822a90000eea000a2d1816e16337ff
Dolev Levy <Dolevlevy97@gmail.com>
2024-05-30 15:04:35 +0300

    Implement local storage

--- Ex - Book Shop/js/book.service.js
+++ Ex - Book Shop/js/book.service.js
@@ -1,44 +1,53 @@
 'use strict'
 
-var gBooks = [
-    {
-        id: 'bg4J78',
-        title: 'Harry Potter and the Deathly Hallows',
-        price: 60,
-        imgUrl: 'book.jpg'
-    },
-    {
-        id: 'ba4cv6',
-        title: 'A Tale of Two Cities',
-        price: 80,
-        imgUrl: 'book.jpg'
-    },
-    {
-        id: 'b8a6r1',
-        title: 'The Little Prince',
-        price: 60,
-        imgUrl: 'book.jpg'
-    }
-]
+var gBooks
+_createBooks()
 
 function getBooks() {
     return gBooks
 }
 
 function removeBook(idx) {
-    gBooks.splice(idx,1)
+    gBooks.splice(idx, 1)
+    _saveBooks()
 }
 
 function updateBook(idx, price) {
     gBooks[idx].price = price
+    _saveBooks()
 }
 
 function addBook(title, price) {
     var newBook = {
-        id:makeId(),
+        id: makeId(),
         title,
         price,
         imgUrl: 'book.jpg'
     }
     gBooks.unshift(newBook)
+    _saveBooks()
+}
+
+function _createBooks() {
+    gBooks = loadFromStorage('books')
+    if (gBooks && gBooks.length !== 0) return
+    gBooks = [
+        _createBook('Harry Potter and the Deathly Hallows', 60),
+        _createBook('A Tale of Two Cities', 80),
+        _createBook('The Little Prince', 60)
+    ]
+    _saveBooks()
+}
+
+function _createBook(title, price) {
+    return {
+        id: makeId(),
+        title,
+        price,
+        imgUrl: 'book.jpg'
+    }
+}
+
+function _saveBooks() {
+    saveToStorage('books', gBooks)
 }
